6bc06befd3e0b9316c24e4fc421a54e1679ba8c4
Ramakrishnareddyvelagala <kittuvelagala@gmail.com>
2024-03-26 13:57:20 -0500

    data inserted.

---
+++ mongodb_insertscript.py
@@ -0,0 +1,43 @@
+# """ 
+# Script to parse course information from courses.json, create the appropriate databases and
+# collection(s) on a local instance of MongoDB, create the appropriate indices (for efficient retrieval)
+# and finally add the course data on the collection(s).
+# """
+
+# import pymongo
+# import json
+
+# # Connect to MongoDB
+# client = pymongo.MongoClient("mongodb://localhost:27017/")
+# db = client["fastapi-courses"]
+# # db_names = client.list_database_names()
+# # for db_name in db_names:
+# #     print(db_name)
+# # quit()
+# collection = db["courses"]
+# # db_collection = db.list_collection_names()
+# # for collection in db_collection:
+# #     print(collection)
+# # quit()
+# # Read courses from courses.json
+# with open("courses.json", "r") as f:
+#     courses = json.load(f)
+    
+# # Create index for efficient retrieval
+# collection.create_index("name")
+
+# # add rating field to each course
+# for course in courses:
+#     course['rating'] = {'total': 0, 'count': 0}
+    
+# # add rating field to each chapter
+# for course in courses:
+#     for chapter in course['chapters']:
+#         chapter['rating'] = {'total': 0, 'count': 0}
+
+# # Add courses to collection
+# for course in courses:
+#     collection.insert_one(course)
+
+# # Close MongoDB connection
+# client.close()
---
+++ readme.txt
@@ -0,0 +1,4 @@
+In this  project, we'll create a Python backend system using FastAPI, a fast web framework, and a MongoDB database for course information storage and retrieval.
+The system will allow users to access course details, view chapters, rate individual chapters, and aggregate ratings.
+
+This is inspired from https://www.freecodecamp.org/news/fastapi-quickstart/
